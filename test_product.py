006bd52c243e051309e01abf71962ee19219b910
Mahwishubham <mazizi4646@email.phoenix.edu>
2024-02-18 19:01:47 -0500

    update testes

--- test_product.py
+++ test_product.py
@@ -6,7 +6,7 @@ def test_product_creation():
     """
     Test that creating a normal product works.
     """
-    product = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    product = Product(product_id='101', name="Bose QuietComfort Earbuds", price=250, quantity=500)
     assert product.name == "Bose QuietComfort Earbuds"
     assert product.price == 250
     assert product.quantity == 500
@@ -17,18 +17,18 @@ def test_invalid_product_creation():
     Test that creating a product with invalid details (empty name, negative price) invokes an exception.
     """
     with pytest.raises(ValueError):
-        Product("", price=250, quantity=500)
+        Product(product_id='101', name="", price=250, quantity=500)
     with pytest.raises(ValueError):
-        Product("Bose QuietComfort Earbuds", price=-250, quantity=500)
+        Product(product_id='101', name="Bose QuietComfort Earbuds", price=-250, quantity=500)
     with pytest.raises(ValueError):
-        Product("Bose QuietComfort Earbuds", price=250, quantity=-500)
+        Product(product_id='101', name="Bose QuietComfort Earbuds", price=250, quantity=-500)
 
 
 def test_product_deactivation():
     """
     Test that when a product reaches 0 quantity, it becomes inactive.
     """
-    product = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    product = Product(product_id='101', name="Bose QuietComfort Earbuds", price=250, quantity=500)
     product.quantity = 0
     assert not product.is_active
 
@@ -37,7 +37,7 @@ def test_product_purchase():
     """
     Test that product purchase modifies the quantity and returns the right output.
     """
-    product = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    product = Product(product_id='101', name="Bose QuietComfort Earbuds", price=250, quantity=500)
     total_price = product.buy(50)
     assert product.quantity == 450
     assert total_price == 250 * 50
@@ -47,6 +47,6 @@ def test_insufficient_quantity_purchase():
     """
     Test that buying a larger quantity than exists invokes exception.
     """
-    product = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    product = Product(product_id="101",name="Bose QuietComfort Earbuds", price=250, quantity=500)
     with pytest.raises(Exception):
         product.buy(600)
